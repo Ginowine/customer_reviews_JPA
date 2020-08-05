c9af3b2bab47c23df2bc24950e69d77246eab59b
ginowine <ginowine1@gmail.com>
2020-08-05 15:44:10 +0100

    database tables created

---
+++ src/main/resources/db/migration/V1_create_db_tables.sql
@@ -0,0 +1,30 @@
+CREATE TABLE comments (
+                          ID int(20) NOT NULL AUTO_INCREMENT,
+                          Text varchar(200) NOT NULL ,
+                          CreatedTime datetime DEFAULT CURRENT_TIMESTAMP,
+                          PRIMARY KEY (ID)
+);
+
+CREATE TABLE reviews(
+                        ID int(20) NOT NULL AUTO_INCREMENT,
+                        ReviewerName varchar(20) NOT NULL ,
+                        ReviewDescription text(200) NOT NULL ,
+                        ReviewTitle varchar(20) NOT NULL ,
+                        CreatedTime datetime DEFAULT current_timestamp,
+                        Comments_ID int(20) NOT NULL,
+                        Product_ID varchar(20) NOT NULL ,
+                        CONSTRAINT fk_product
+                        FOREIGN KEY (Product_ID) REFERENCES products(ID) ON DELETE CASCADE ,
+
+                        CONSTRAINT fk_comment
+                        FOREIGN KEY (Comments_ID) REFERENCES comments(ID) ON DELETE CASCADE,
+                        PRIMARY KEY (ID)
+);
+
+CREATE TABLE products(
+                         ID bigint(20) NOT NULL AUTO_INCREMENT,
+                         ProductName varchar(20) NOT NULL ,
+                         ProductAmount double(20) NOT NULL ,
+                         PRIMARY KEY (ID)
+);
+
