e4db7bf935f1708e362a730a79f74a25ae3d0eb9
ginowine <ginowine1@gmail.com>
2020-08-18 14:12:43 +0100

    review repository test created

--- src/test/java/com/udacity/course3/reviews/ProductRepositoryTest.java
+++ src/test/java/com/udacity/course3/reviews/ProductRepositoryTest.java
@@ -6,36 +6,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 import static junit.framework.TestCase.assertEquals;
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class ProductRepositoryTest {
 
-    @Autowired private DataSource dataSource;
-    @Autowired private JdbcTemplate jdbcTemplate;
-    @Autowired private EntityManager entityManager;
-    @Autowired private TestEntityManager testEntityManager;
     @Autowired private ProductRepository productRepository;
-
 
     @Test
     public void testFindProductById(){
         Product product = new Product();
-
         product.setId(1L);
         product.setProduct_Name("Gucci Bag");
         product.setProduct_Amt(400.00);
-
-        //entityManager.merge(product);
         productRepository.save(product);
 
         Product actualProduct = productRepository.findProductById(product.getId());
@@ -43,5 +31,27 @@ public class ProductRepositoryTest {
         assertEquals(product.getId(), actualProduct.getId());
         assertEquals(product.getProduct_Name(), actualProduct.getProduct_Name());
         assertEquals(product.getProduct_Amt(), actualProduct.getProduct_Amt());
+    }
+
+    @Test
+    public void testDeleteProductById(){
+        Product product = new Product();
+        product.setId(3L);
+        product.setProduct_Name("jhgfkj khkjfdhk");
+        product.setProduct_Amt(600.00);
+
+        Product product1 = productRepository.save(product);
+        productRepository.deleteById(product1.getId());
+    }
+
+    @Test
+    public void getAllProducts(){
+        Product product = new Product();
+        product.setId(2L);
+        product.setProduct_Name("Doggy Dog");
+        product.setProduct_Amt(500.00);
+
+        productRepository.save(product);
+        assertNotNull(productRepository.findAll());
     }
 }
